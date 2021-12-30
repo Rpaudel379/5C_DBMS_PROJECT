660b0aa75429a0dbe69e10d6694f4cde76c20563
Anish Paudel <rpaudel379@gmail.com>
2021-12-30 09:54:33 +0530

    fixed bugs 2

--- components/header.js
+++ components/header.js
@@ -40,7 +40,7 @@ const Header = () => {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="https://www.github.com/rpaudel379"
+                    href="https://github.com/Rpaudel379/5C_DBMS_PROJECT"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
                     View code on Github
